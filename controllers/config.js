1c007b69fbbf9c1dc7d6769913716d386d11d343
Bensu <bnsue1@gmail.com>
2018-03-02 19:43:53 -0500

    First commit

---
+++ controllers/config.js
@@ -0,0 +1,7 @@
+// The domain and port on which the app is running
+var host = process.env.HOST || 'http://localhost:5000'
+module.exports.host = host
+
+// The domain and port on which the app is running
+var port = process.env.PORT || 500
+module.exports.port = port
---
+++ index.js
@@ -0,0 +1,75 @@
+// var cool = require('cool-ascii-faces');
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+var session = require('express-session');
+var CASAuthentication = require('cas-authentication');
+var config = require('./controllers/config.js')
+ 
+// Set up an Express session, which is required for CASAuthentication. 
+app.use( session({
+    secret            : 'super secret key',
+    resave            : false,
+    saveUninitialized : true
+}));
+ 
+// Create a new instance of CASAuthentication. 
+var cas = new CASAuthentication({
+    cas_url     : 'https://fed.princeton.edu/cas/',
+    service_url : config.host,
+    cas_version: "saml1.1"
+});
+ 
+// Unauthenticated clients will be redirected to the CAS login and then back to 
+// this route once authenticated. 
+app.get('/login', cas.bounce, function ( req, res ) {
+    res.send( '<html><body>Hello, ' +  res.netid + '!</body></html>' );
+});
+ 
+// Unauthenticated clients will receive a 401 Unauthorized response instead of 
+// the JSON data. 
+app.get( '/api', cas.block, function ( req, res ) {
+    res.json( { success: true } );
+});
+ 
+// An example of accessing the CAS user session variable. This could be used to 
+// retrieve your own local user records based on authenticated CAS username. 
+app.get( '/api/user', cas.block, function ( req, res ) {
+    res.json( { cas_user: req.session[ cas.session_name ] } );
+});
+ 
+// Unauthenticated clients will be redirected to the CAS login and then to the 
+// provided "redirectTo" query parameter once authenticated. 
+app.get( '/authenticate', cas.bounce_redirect );
+ 
+// This route will de-authenticate the client with the Express server and then 
+// redirect the client to the CAS logout page. 
+app.get( '/logout', cas.logout );
