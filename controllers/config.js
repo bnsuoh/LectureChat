2371d3b65376bb4284b8e2c549523cef3aa37efe
Bensu <bnsue1@gmail.com>
2018-03-23 00:05:52 -0400

    fixing url bug

--- controllers/config.js
+++ controllers/config.js
@@ -1,5 +1,5 @@
 // The domain and port on which the app is running
-var host = process.env.HOST || 'http://localhost:5000'
+var host = /*process.env.HOST*/ location.hostname || 'http://localhost:5000'
 module.exports.host = host
 
 // The domain and port on which the app is running
